220f3c06233f682b3eb226dd37a07380b4f2724e
Siddharth Umajwal <siddharthmeenasiddhu@gmail.com>
2026-05-21 15:48:23 +0530

    fix: add material symbols to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+      </head>
       <body className={`${notoSans.variable} font-sans antialiased`}>
         <LoadingScreen />
         <Providers>{children}</Providers>
